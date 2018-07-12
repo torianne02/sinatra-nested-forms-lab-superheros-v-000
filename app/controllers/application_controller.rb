6474cc11ae26e16b659412db553ad21edfdd1432
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-12 20:02:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/team' do
+      @teams = Team.new(params[:team])
+
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+      end
+
+      @heroes = Superhero.all
+
+      erb :team
+    end
 end
