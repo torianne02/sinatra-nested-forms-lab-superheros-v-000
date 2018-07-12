975fc2c033e963bd5fb939b5adbd5fdd61d35d74
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-12 20:03:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
     erb :super_hero
   end
 
-  post '/team' do
+  post '/teams' do
     @teams = Team.new(params[:team])
 
     params[:team][:superheroes].each do |details|
